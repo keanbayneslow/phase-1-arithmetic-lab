d5143927759346a7e3dffd667103fa04bb22641d
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-08-13 15:54:50 +0800

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 17;
+let num4 = 13;
+let numbers = [5, 10, 20, 15];
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(...numbers);
